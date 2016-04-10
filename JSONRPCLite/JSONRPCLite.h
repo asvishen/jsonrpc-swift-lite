35bfdfeda7433b56b11e1f2fa767797bb103f499
avijitvishen <avishen@asu.edu>
2016-04-09 21:33:59 -0700

    Initial Commit

---
+++ JSONRPCLite/JSONRPCLite.h
@@ -0,0 +1,19 @@
+//
+//  JSONRPCLite.h
+//  JSONRPCLite
+//
+//  Created by Avijit Vishen on 4/9/16.
+//  Copyright © 2016 Avijit Vishen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JSONRPCLite.
+FOUNDATION_EXPORT double JSONRPCLiteVersionNumber;
+
+//! Project version string for JSONRPCLite.
+FOUNDATION_EXPORT const unsigned char JSONRPCLiteVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JSONRPCLite/PublicHeader.h>
+
+
