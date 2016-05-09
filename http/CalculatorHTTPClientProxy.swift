5b2bebc3c880cf1a1c3d1791bdca8fe2c7567397
Avijit Singh Vishen <avijitsinghvishen@gmail.com>
2016-05-09 08:02:58 -0700

    Delete CalculatorHTTPClientProxy.swift

--- http/CalculatorHTTPClientProxy.swift
+++
@@ -1,9 +0,0 @@
-//
-//  CalculatorHTTPClientProxy.swift
-//  JSONRPCLite
-//
-//  Created by Avijit Vishen on 5/9/16.
-//  Copyright © 2016 Avijit Vishen. All rights reserved.
-//
-
-import Foundation
